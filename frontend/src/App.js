45b5ecda106ae5ee9d67311ff974baaf83087987
Jahanban <jahanban@gmail.com>
2024-05-19 00:05:55 -0400

    Added unspalsh api key to env.local

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
 import Search from "./components/search";
 
+const UNSPALSH_KEY = process.env.REACT_APP_UNSPALSH_KEY;
 function App() {
   const [word, setWord] = useState("");
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
   };
-  console.log(word);
+  // console.log(process.env.REACT_APP_UNSPALSH_KEY);
   return (
     <div className="App">
       <Header title="Images Gallery"></Header>
