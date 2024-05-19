e4714ab2dac64bf8b49dd5ec9ba0329aa9a9f17f
Jahanban <jahanban@gmail.com>
2024-05-18 23:38:14 -0400

    Search is now a controlled component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
 import Search from "./components/search";
 
 function App() {
+  const [word, setWord] = useState("");
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+  console.log(word);
   return (
     <div className="App">
       <Header title="Images Gallery"></Header>
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
