013c9e586471f20e38549871f5313e18a35e88de
Jahanban <jahanban@gmail.com>
2024-05-19 00:26:05 -0400

    First api call made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
 import Search from "./components/search";
 
-const UNSPALSH_KEY = process.env.REACT_APP_UNSPALSH_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPALSH_KEY;
+
 function App() {
   const [word, setWord] = useState("");
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(
+      `https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   // console.log(process.env.REACT_APP_UNSPALSH_KEY);
   return (
