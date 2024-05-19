23f266c124dccd03f310fc1c8e35753b2f9b9511
Jahanban <jahanban@gmail.com>
2024-05-18 23:08:15 -0400

    Added search input and button in search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
+import Search from "./components/search";
 
 function App() {
   return (
     <div className="App">
       <Header title="Images Gallery"></Header>
+      <Search />
     </div>
   );
 }
